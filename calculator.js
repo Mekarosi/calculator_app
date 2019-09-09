43295311490f312426697231106a600a5c5301c0
Mekarosi Assa <mekarosi_assa@yahoo.com>
2019-09-09 18:21:04 +0100

    Modify calculator view

--- calculator.js
+++ calculator.js
@@ -11,3 +11,28 @@ function updateDisplay() {
 }
 
 updateDisplay();
+
+const keys = document.querySelector('.btn');
+keys.addEventListener('click', event => {
+  const { target } = event;
+  if (!target.matches('btn')) {
+    return;
+  }
+
+  if (target.classList.contains('btn orange')) {
+    console.log('operator', target.value);
+    return;
+  }
+
+  if (target.classList.contains('btn decimal')) {
+    console.log('decimal', target.value);
+    return;
+  }
+
+  if (target.classList.contains('btn clear')) {
+    console.log('clear', target.value);
+    return;
+  }
+
+  console.log('digit', target.value);
+});
